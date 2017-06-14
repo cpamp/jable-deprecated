16c3d3748761da68049923926d82b71b77b00eb0
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-06-13 22:36:47 -0500

    fix: use descriptor for old func

--- src/DeprecatedMethod.ts
+++ src/DeprecatedMethod.ts
@@ -2,7 +2,7 @@ import { logDeprecated } from "./logDeprecated";
 
 export function DeprecatedMethod() {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        var oldFunc = target[propertyKey];
+        var oldFunc = descriptor.value;
         descriptor.value = function() {
             logDeprecated(target.constructor.name + '.' + propertyKey);
             return oldFunc.apply(this, arguments);
